d82bb166f61dac7631c9e2c16dc72282e9f47d3a
michael-maia <michaelmdasilva@yahoo.com.br>
2021-06-10 18:18:39 -0300

    Add project files.

---
+++ Framework_API/Data/DBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Framework_API.Data
+{
+    public class DBContext : IdentityDbContext
+    {
+        public DBContext(DbContextOptions<DBContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Framework_API/Startup.cs
@@ -0,0 +1,86 @@
+using Framework_API.Data;
+using Framework_API.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Framework_API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<DBContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DBConnection")));
+            services.AddIdentity<User, Role>(options => options.SignIn.RequireConfirmedAccount = true).AddDefaultUI().AddEntityFrameworkStores<DBContext>();
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                // Cookie sera somente via HTTP
+                options.Cookie.HttpOnly = true;
+                // Tempo de expiracao do Cookie
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(50);
+                // URL do login
+                options.LoginPath = "/Users/Login/";
+                // Gera um novo cookie ao expirar o antigo
+                options.SlidingExpiration = true;
+            });
+
+            // Define os parametros para uma senha aceitavel (funcionalidade do Identity)
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 6;
+                options.Password.RequiredUniqueChars = 1;
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
