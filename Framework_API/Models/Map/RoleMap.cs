5a94587370ce5ef8c824b515b5055e622bfcf59f
michael-maia <michaelmdasilva@yahoo.com.br>
2021-06-14 19:06:17 -0300

    Mapeamento da classe Role

---
+++ Framework_API/Models/Map/RoleMap.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Framework_API.Models
+{
+    public class RoleMap : IEntityTypeConfiguration<Role>
+    {
+        // Mapeamento da classe Role
+        public void Configure(EntityTypeBuilder<Role> builder)
+        {
+            // Definindo as configurações das propriedades para o banco de dados
+            builder.Property(u => u.Name).IsRequired().HasMaxLength(20);
+        }
+    }
+}
